17d423225ac17dd1f724d81ace4fddb4b7c4bb4e
xianggu625 <xianggu625@126.com>
2020-02-02 17:46:49 +0800

    Add files via upload

--- src/main/java/com/jerry/mytest/Mytest.java
+++ src/main/java/com/jerry/mytest/Mytest.java
@@ -3,9 +3,9 @@ package com.jerry.mytest;
 public class Mytest {
 	public void judge(int n) {
         if(n==0){
-			return 0;
+			System.out.println("0");
 		}else if(n==1){
-			return 1;
+			System.out.println("1");
 		}
     }
 }
